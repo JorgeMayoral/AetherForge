bc33b6962cf6a32b48174f36b4f12f1ba0b80260
Jorge Mayoral Álvarez <jorge.ma@yorch.dev>
2025-06-30 19:47:53 +0200

    feat(nixos): add nixos config

---
+++ nixos/flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "Aether Forge flake";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+  outputs =
+    { nixpkgs, ... }@inputs:
+    {
+      nixosConfigurations = {
+        aether = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            ./configuration.nix
+          ];
+        };
+      };
+    };
+}
