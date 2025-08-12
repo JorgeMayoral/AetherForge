f3c2779609617d52e1565bc49b7d8fa926d9cbf1
Jorge Mayoral Álvarez <jorge.ma@yorch.dev>
2025-08-12 18:29:49 +0200

    feat(nixos): add home-manager

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -2,9 +2,13 @@
   description = "Aether Forge flake";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
   outputs =
-    { nixpkgs, ... }@inputs:
+    { nixpkgs, home-manager, ... }@inputs:
     {
       nixosConfigurations = {
         aether = nixpkgs.lib.nixosSystem {
@@ -12,6 +16,12 @@
           modules = [
             ./configuration.nix
             /etc/nixos/hardware-configuration.nix
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.yorch = import ./home.nix;
+            }
           ];
         };
       };
---
+++ nixos/home.nix
@@ -0,0 +1,59 @@
+{ config, pkgs, ... }:
+{
+  home.username = "yorch";
+  home.homeDirectory = "/home/yorch";
+
+  home.packages = with pkgs; [
+    delta
+    gh
+    stow
+    bat
+    fzf
+    erdtree
+    eza
+    starship
+    tealdeer
+    helix
+    nushell
+    xh
+    yazi
+    zellij
+    glow
+    git-cliff
+    wezterm
+    ghostty
+    vscode
+    unzip
+    bottom
+    kubectl
+    kubectx
+    kubeseal
+    kubernetes-helm
+    k9s
+    lefthook
+    devenv
+    # nodejs_24
+    nil
+    nixfmt-rfc-style
+    rustup
+    gcc
+    uv
+    fastfetch
+    libnotify
+    ripgrep
+    google-chrome
+    libreoffice
+    gimp3
+    obsidian
+    papers
+    simple-scan
+    gnome-tweaks
+    gnomeExtensions.blur-my-shell
+    gnomeExtensions.tophat
+    gnomeExtensions.space-bar
+    gnomeExtensions.tweaks-in-system-menu
+    gnomeExtensions.caffeine
+  ];
+
+  home.stateVersion = "25.11";
+}
